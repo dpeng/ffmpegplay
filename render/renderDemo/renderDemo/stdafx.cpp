461f60d5626acc7960b83bce88f36ce6fbd155ac
sensefeng <sensefeng@gmail.com>
2013-04-05 09:22:52 +0000

    delete unused code

--- render/renderDemo/renderDemo/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// renderDemo.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
